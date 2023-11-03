13d61c022c8e8bb1f22b2bcafb6af9cda82923e9
Esteban <estebanrmm88@gmail.com>
2023-11-03 16:23:40 -0600

    se agrego draggable.js

--- js/weekly-weather.js
+++ js/weekly-weather.js
@@ -3,6 +3,7 @@ import { getLatLon } from './utils/geolocation.js'
 import { formatWeekList } from './utils/format-data.js'
 import { createDOM } from './utils/dom.js'
 import { createPeriodTime } from './period-time.js'
+import draggable from './draggable.js'
 
 
 function tabPanelTemplate(id) {
@@ -27,7 +28,6 @@ function createTabPanel(id) {
 }
 
 function configWeeklyWeather(weeklist) {
-    //const $container = document.querySelector('.weeklyWeather')
     const $container = document.querySelector('.tabs')
     weeklist.forEach((day, index) => {
         const $panel = createTabPanel(index)
@@ -36,14 +36,16 @@ function configWeeklyWeather(weeklist) {
             $panel.querySelector('.dayWeather-list').append(createPeriodTime(weather))
         } )
     })
-
+    
 }
 
 export default async function weeklyWeather () {
+    const $container = document.querySelector('.weeklyWeather')
     const { lat, lon, isError } = await getLatLon()
     if (isError) return console.log('ha ocurrido un error ubicandote')
    const { isError: weeklyWeatherError, data: weather } = await getWeeklyWeather (lat, lon)
    if (weeklyWeatherError) return console.log('oh! ha ocurrido un error trayendo el pronostico del clima')
     const weeklist = formatWeekList(weather.list)
     configWeeklyWeather(weeklist)
+    draggable($container)
 }
